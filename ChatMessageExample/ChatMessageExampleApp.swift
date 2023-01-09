b6d1bc4112c4ada8ba609ef9e59c9d0396049214
katafuchix <katafuchix@gmail.com>
2023-01-09 21:07:30 +0900

    Initial Commit

---
+++ ChatMessageExample/ChatMessageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMessageExampleApp.swift
+//  ChatMessageExample
+//
+//  Created by cano on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct ChatMessageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
